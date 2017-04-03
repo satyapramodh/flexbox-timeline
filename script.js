92079d0fded59741317c10a8928cfb8ef3b72a25
Satya Pramodh Mazumdar (Student) <smazumd5@en4090417.mobile.asu.edu>
2017-04-03 14:39:37 -0700

    made a working version of 7 cards

---
+++ script.js
@@ -0,0 +1,27 @@
+$(function () {
+  // Get demo-card-wrapper height
+  // $(".demo-card-wrapper").height(
+  //
+  //   function(){
+  //     // return _.reduce(
+  //     //   $(".demo-card"),
+  //     //   function(sum, card){
+  //     //     return sum + $(card).height()
+  //     //   },
+  //     //   400
+  //     // );
+  //     console.log("height", $(".demo-card-wrapper").height(), $(".demo-card").last().height());
+  //     return $(".demo-card-wrapper").height() + $(".demo-card").last().height()
+  //   }
+  // );
+
+  // _.each($(".demo-card"), function(card){
+  //   $(".demo-card-wrapper").height(
+  //     function(){
+  //       console.log("height", $(".demo-card-wrapper").height(), $(card).height());
+  //       return $(".demo-card-wrapper").height() + $(card).height() + 90 + 50;
+  //     }
+  //   );
+  // })
+
+})
