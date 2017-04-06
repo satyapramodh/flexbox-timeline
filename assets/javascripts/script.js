c90066b9bc4919b38e49613d790c321f7b0c2446
Satya Pramodh Mazumdar (Student) <smazumd5@en4090417.mobile.asu.edu>
2017-04-06 16:23:58 -0700

    updated on scroll effect

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,69 +1,23 @@
-function setCardStyles() {
-  $.each($(".card"), function (idx, card) {
-    // rotate head-color-class between 0-8
-    $(card).find(".head").addClass("head-color-" + idx%8);
-  });
-}
-
-function animateCards() {
-
-  if (window.cardData.data.length < window.cardCount){
-    return
+function addNextCard() {
+  if( cardCount >= cardData.data.length){
+    return;
   }
-
-  var windowHeight = $(window).height();
-  var scrollHeight = $(window).scrollTop();
-  // console.log("scrollHeight", scrollHeight);
-  // $.each(data, function(idx, card){
-  //     // console.log("idx, position, bool", idx, $(card).position().top + 400 - scrollHeight, $(card).position().top + 400 -  scrollHeight < 600);
-  //     var position = $($(".card")[idx]).position();
-  //
-  //     console.log("idx, position", idx, position);
-  //     // debugger;
-  //     if( (position.top + 400 - scrollHeight) < (windowHeight*0.6) ){
-  //       var effectClass = ((idx%2 === 0) ? "bounceInLeft" : "bounceInRight");
-  //       console.log("windowHeight, scrollHeight, position", windowHeight, scrollHeight, position.top);
-  //       $(card).removeClass("hide-card animated bounceInLeft bounceInRight").addClass("animated " + effectClass);
-  //     }
-  // });
-  console.log("cardCount", window.cardCount);
-  $(".timeline .wrapper").append(window.cardData.data[window.cardCount]);
-  $(".card").last().addClass("bounceInLeft");
-  window.cardCount++;
-  setCardStyles();
+  
+  $(".timeline .wrapper").append(cardData.data[cardCount]);
+  $(".card").last().addClass(
+    ((cardCount%2 === 0) ? "bounceInLeft" : "bounceInRight")
+  );
+  cardCount++;
+  $(".card").last().find(".head").addClass("head-color-" + cardCount%8);
 }
 
 $(function () {
-  // Get demo-card-wrapper height
-  // $(".demo-card-wrapper").height(
-  //
-  //   function(){
-  //     // return _.reduce(
-  //     //   $(".demo-card"),
-  //     //   function(sum, card){
-  //     //     return sum + $(card).height()
-  //     //   },
-  //     //   400
-  //     // );
-  //     console.log("height", $(".demo-card-wrapper").height(), $(".demo-card").last().height());
-  //     return $(".demo-card-wrapper").height() + $(".demo-card").last().height()
-  //   }
-  // );
+  cardCount = 0;
 
-  // _.each($(".demo-card"), function(card){
-  //   $(".demo-card-wrapper").height(
-  //     function(){
-  //       console.log("height", $(".demo-card-wrapper").height(), $(card).height());
-  //       return $(".demo-card-wrapper").height() + $(card).height() + 90 + 50;
-  //     }
-  //   );
-  // })
-
-  setCardStyles();
-  window.cardCount = 0;
-
-  window.cardData = {
+  cardData = {
       "data": [
+        "<div class='card animated'><div class='head'><div class='number-box'><span>01</span></div><h2><span class='small'>Subtitle</span> Technology</h2></div><div class='body'><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p><img src='http://lorempixel.com/1000/500' alt='Graphic'></div></div>",
+        "<div class='card animated'><div class='head'><div class='number-box'><span>02</span></div><h2><span class='small'>Subtitle</span> Confidence</h2></div><div class='body'><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p><iframe width='100%' height='315' src='https://www.youtube.com/embed/DiTECkLZ8HM' frameborder='0' allowfullscreen></iframe></div></div>",
         "<div class='card animated'><div class='head'><div class='number-box'><span>03</span></div><h2><span class='small'>Subtitle</span> Confidence</h2></div><div class='body'><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p><iframe width='100%' height='315' src='https://www.youtube.com/embed/DiTECkLZ8HM' frameborder='0' allowfullscreen></iframe></div></div>",
         "<div class='card animated'><div class='head'><div class='number-box'><span>04</span></div><h2><span class='small'>Subtitle</span> Confidence</h2></div><div class='body'><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p><iframe width='100%' height='315' src='https://www.youtube.com/embed/DiTECkLZ8HM' frameborder='0' allowfullscreen></iframe></div></div>",
         "<div class='card animated'><div class='head'><div class='number-box'><span>01</span></div><h2><span class='small'>Subtitle</span> Technology</h2></div><div class='body'><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta reiciendis deserunt doloribus consequatur, laudantium odio dolorum laboriosam.</p><img src='http://lorempixel.com/1000/500' alt='Graphic'></div></div>",
@@ -82,10 +36,10 @@ $(function () {
       ]
   };
 
-  animateCards();
+  addNextCard();
+  addNextCard();
+  addNextCard();
 
-  $(window).scroll(function () {
-    animateCards();
-  });
+  $('.timeline').timelify();
 
 });
---
+++ assets/javascripts/timelify.js
@@ -0,0 +1,34 @@
+$.fn.isVisible = function(offset) {
+	// Am I visible?
+	// Height and Width are not explicitly necessary in visibility detection, the bottom, right, top and left are the
+	// essential checks. If an image is 0x0, it is technically not visible, so it should not be marked as such.
+	// That is why either width or height have to be > 0.
+	var rect = this[0].getBoundingClientRect();
+	return (
+		(rect.height > 0 || rect.width > 0) &&
+		rect.bottom >= 0 &&
+		rect.right >= 0 &&
+		rect.top <= (window.innerHeight - offset || document.documentElement.clientHeight - offset) &&
+		rect.left <= (window.innerWidth - offset || document.documentElement.clientWidth - offset)
+	);
+};
+
+$.fn.timelify = function(options){
+
+	var elem = this;
+
+	// old scroll pos helps determine the 'scrolled' height between the
+	// add-card-interaction and limit scroll event to trigger adding all
+	// cards at once
+	oldScrollPos = 0;
+	window.addEventListener('scroll', function(){
+		var scrollPos = $(window).scrollTop();
+		if((scrollPos > $(elem).offset().top - 600)  && Math.abs(oldScrollPos - scrollPos) >  $(window).height()*0.2){
+			oldScrollPos = scrollPos;
+			addNextCard();
+		}
+
+	});
+
+	return this;
+};
